32063d00217842daecad409c661ebb68dea8c287
NebiyuBirhane <nebiyubirhaneab@gmail.com>
2023-08-11 03:07:36 -0500

    python2

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add(a, b):
+	"""Function
+		a:first number
+		b:second number
+		return the result
+		"""
+		return (a + b)
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Add function 
+Substraction 
+Product
+Division
+"""
+def add (a, b):
+	return (a + b)
+def sub(a, b):
+	return (a - b)
+def mul(a, b):
+       return (a * b)
+	def div(a, b):
+		return int(a / b)
